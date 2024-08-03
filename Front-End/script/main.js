11f9c0c74e4da20e4bce22fbf8a19c3cc93031a4
marsodeb <marceaud.pro@gmail.com>
2024-08-03 20:22:47 +0200

    working on works upload + previsualisation

--- Front-End/script/main.js
+++ Front-End/script/main.js
@@ -46,12 +46,16 @@ function checkAdminStatus() {
 function openModal() {
     document.querySelector(".admin-modif").addEventListener('click', () => {
         const modal = document.querySelector(".modal");
-        const modalButton = document.querySelector(".modal-button");
+        const modalButton = document.querySelector(".modal-first .modal-button");
         const modalTitle = document.querySelector(".modal-title");
         const modalBack = document.querySelector(".modal-back");
         const modalUpload = document.querySelector(".modal-second");
         const modalUploadTitle = document.querySelector(".modal-second .modal-title");
-        const modalUploadButton = document.querySelector(".modal-second .modal-button");
+        const modalUploadButton = document.querySelector(".modal-second .modal-button #uploadBtn");
+        const modalUploadCategories = document.getElementById("imageCategory");
+        const previewImage = document.getElementById('previewImage');
+        const imageFileInput = document.getElementById('imageFile');
+        const labelPhoto = document.getElementById('labelPhoto');
         if (!modal) {
             console.error("L'élément '.modal' est introuvable.");
             return;
@@ -63,13 +67,47 @@ function openModal() {
         modalButton.addEventListener("click", function () {
             modalUpload.style.display = "flex";
             modalUploadTitle.innerHTML = "<h2>Ajout photo</h2>";
-            modalUploadButton.innerHTML = "<button id='addPhotoBtn'>Valider</button>";
+            modalUploadCategories.innerHTML = "";
+            for (var i = 0; i < categorie.length; i++) {
+                const uploadCategories = document.createElement("option")
+                uploadCategories.setAttribute("value", categorie[i].id)
+                uploadCategories.innerText = categorie[i].name;
+                modalUploadCategories.appendChild(uploadCategories);
+            }
+        })
+        modalUploadButton.addEventListener("click", function () {
+            if (modalUploadButton.classList != "btn-not-selected") {
+                uploadModal();
+            } else {
+                showPopup("Vous devez d'abord compléter le formulaire d'upload.", true);
+            }
         })
 
         modalBack.addEventListener("click", function () {
             modalUpload.style.display = "none";
+            modalUploadCategories.innerHTML = "";
+            if (previewImage.src != "") {
+                previewImage.src = "";
+                labelPhoto.style.display = 'flex';
+                modalUploadButton.classList = 'btn-not-selected';
+            }
         })
 
+        imageFileInput.addEventListener('change', (event) => {
+            const file = event.target.files[0]; // Prendre le premier fichier sélectionné
+            if (file && file.type.startsWith('image/')) {
+                const reader = new FileReader();
+                reader.onload = (e) => {
+                    previewImage.src = e.target.result;
+                    previewContainer.style.display = 'block';
+                    labelPhoto.style.display = 'none';
+                    modalUploadButton.classList = '';
+                };
+                reader.readAsDataURL(file); // Lire le fichier comme une URL de données
+            } else {
+                showPopup("Le fichier n'est pas une image.", true);
+            }
+        });
         genererWorksModal(works);
         deletWorks();
         exitModal();
@@ -79,15 +117,60 @@ function openModal() {
 function exitModal() {
     const modalExit = document.querySelectorAll(".modal-exit");
     const modal = document.querySelector(".modal");
+    const modalUpload = document.querySelector(".modal-second");
+    const modalUploadCategories = document.getElementById("imageCategory");
+    const previewImage = document.getElementById('previewImage');
+    const labelPhoto = document.getElementById('labelPhoto');
+    const modalUploadButton = document.querySelector(".modal-second .modal-button #uploadBtn");
     modalExit.forEach(close => {
         close.addEventListener("click", function () {
             if (!modalExit) {
                 console.error("L'élément '.modal-exit' est introuvable.");
                 return;
             }
+            if (previewImage.src != "") {
+                previewImage.src = "";
+                labelPhoto.style.display = 'flex';
+                modalUploadButton.classList = 'btn-not-selected';
+            }
             modal.style.display = "none";
+            modalUpload.style.display = "none";
+            modalUploadCategories.innerHTML = "";
         })
     })
+}
+
+function uploadModal() {
+    const form = document.getElementById("uploadForm");
+    const formData = new FormData(form);
+    const previewImage = document.getElementById('previewImage');
+    const labelPhoto = document.getElementById('labelPhoto');
+    const modalUploadButton = document.querySelector(".modal-second .modal-button #uploadBtn");
+    fetch('http://localhost:5678/api/works', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+        },
+        body: formData
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Upload successful:', data);
+            fetchData().then(() => {
+                genererWorks(works);
+                genererWorksModal(works);
+                deletWorks();
+                const modalUpload = document.querySelector(".modal-second");
+                modalUpload.style.display = "none";
+                previewImage.src = "";
+                labelPhoto.style.display = 'flex';
+                modalUploadButton.classList = 'btn-not-selected';
+                showPopup("Travail ajouté avec succès.");
+            });
+        })
+        .catch(error => {
+            showPopup("Erreur lors de l'upload.", true);
+        });
 }
 
 function deletWorks() {
